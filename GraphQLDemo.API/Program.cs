759477326f77ee19c261cf8ef59b5f3c9eb2d9d7
Igor Santana Medeiros <1434@ucdb.br>
2026-05-25 09:02:27 -0400

    Initial commit

---
+++ GraphQLDemo.API/Program.cs
@@ -0,0 +1,14 @@
+using GraphQLDemo.API.Schema;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL();
+
+app.Run();
