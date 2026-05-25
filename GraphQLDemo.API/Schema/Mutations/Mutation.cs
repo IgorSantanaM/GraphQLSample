4ac4104338fa847c03590208b59b4e39f04fb76c
Igor Santana Medeiros <1434@ucdb.br>
2026-05-25 09:56:14 -0400

    (feat): Added mutations

---
+++ GraphQLDemo.API/Schema/Mutations/Mutation.cs
@@ -0,0 +1,50 @@
+﻿using GraphQLDemo.API.Schema.Queries;
+
+namespace GraphQLDemo.API.Schema.Mutations
+{
+    public class Mutation
+    {
+        private readonly List<CourseResult> _courses;
+        public Mutation()
+        {
+            _courses = new List<CourseResult>();
+        }
+
+        public CourseResult CreateCourse(CourseInputType courseInput)
+        {
+
+            CourseResult course = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = courseInput.Name,
+                Subject = courseInput.Subject,
+                InstructorId = courseInput.InstructorId
+            };
+
+            _courses.Add(course);
+
+            return course;
+        }
+
+        public CourseResult UpdateCourse(Guid id, CourseInputType courseInput)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == id);
+
+            if (course is null)
+            {
+                throw new GraphQLException("Course not found");
+            }
+
+            course.Name = courseInput.Name;
+            course.Subject = courseInput.Subject;
+            course.InstructorId = courseInput.InstructorId;
+
+            return course;
+        }
+
+        public bool DeleteCourse(Guid id)
+        {
+            return _courses.RemoveAll(c => c.Id == id) >= 1; 
+        }
+    }
+}
--- GraphQLDemo.API/Schema/Queries/InstructorType.cs
+++ GraphQLDemo.API/Schema/Queries/InstructorType.cs
@@ -1,11 +1,10 @@
-﻿namespace GraphQLDemo.API.Schema
+﻿namespace GraphQLDemo.API.Schema.Queries
 {
-    public class StudentType
+    public class InstructorType
     {
         public Guid Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        [GraphQLName("gpa")]
-        public double? GPA { get; set; }
+        public double? Salary { get; set; }
     }
 }
