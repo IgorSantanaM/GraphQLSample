a2a3e3345a5d0e4f07e09ddae708799496d56ee2
Igor Santana Medeiros <1434@ucdb.br>
2026-05-26 14:47:18 -0400

    (feat): Added Pagination and Filtering

---
+++ GraphQLDemo.API/DataLoaders/InstructorDataLoader.cs
@@ -0,0 +1,25 @@
+﻿using GraphQLDemo.API.DTOs;
+using GraphQLDemo.API.Services.Instructors;
+
+namespace GraphQLDemo.API.DataLoaders
+{
+    public class InstructorDataLoader : BatchDataLoader<Guid, InstructorDTO>
+    {
+        private readonly InstructorsRepository _instructorsRepository;
+
+        public InstructorDataLoader(IBatchScheduler batchScheduler, 
+            DataLoaderOptions options, 
+            InstructorsRepository instructorsRepository)
+            : base(batchScheduler, options)
+        {
+            _instructorsRepository = instructorsRepository;
+        }
+
+        protected override async Task<IReadOnlyDictionary<Guid, InstructorDTO>> LoadBatchAsync(IReadOnlyList<Guid> keys, CancellationToken cancellationToken)
+        {
+            var instructors = await _instructorsRepository.GetManyByIds(keys);
+
+            return instructors.ToDictionary(i => i.Id);
+        }
+    }
+}
--- GraphQLDemo.API/Services/Courses/CoursesRepository.cs
+++ GraphQLDemo.API/Services/Courses/CoursesRepository.cs
@@ -46,5 +46,23 @@ namespace GraphQLDemo.API.Services.Courses
                 return await context.SaveChangesAsync() > 0;
             }
         }
+
+        public async Task<IEnumerable<CourseDTO>> GetAll()
+        {
+            using (SchoolDbContext context = _dbContextFactory.CreateDbContext())
+            {
+                return await context.Courses
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<CourseDTO?> GetById(Guid courseId)
+        {
+            using (SchoolDbContext context = _dbContextFactory.CreateDbContext())
+            {
+                return await context.Courses
+                    .FirstOrDefaultAsync(c => c.Id == courseId);
+            }
+        }
     }
 }
